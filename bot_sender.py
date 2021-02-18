d083adb11c68fcdd05418d4699717da005dfc627
TAPAKAHOKOT <igrok332233@mail.ru>
2021-02-19 01:45:51 +0300

    Added binance info regular updating

--- bot_sender.py
+++ bot_sender.py
@@ -14,7 +14,12 @@ import time
 import codecs
 from threading import Thread
 from datetime import datetime
+from binance.client import Client
 
+api_key = 'd0uy6vPg0CbCsPOSAZOborpqMJMWmLwp1trqa8Mz8UeobliYpBTnmZWBauoQZsrc'
+api_secret = 'MPDLBUCjFVhYq6zcytoAxvJNX98ilaBbDXbO1ql9RSBla9mC0exMLk9NvANnD3yg'
+
+client = Client(api_key, api_secret)
 
 my_id = 472914986
 
@@ -39,7 +44,9 @@ def write_into_file():
 	with codecs.open("send_data.txt", encoding = 'utf-8', mode = 'w') as file:
 		file.write(settings_info_line)
 
-		line = "-\n" + ", ".join(cities_history)
+		line = "-1\n" + ", ".join(cities_history)
+		file.write(line)
+		line = "\n-2\n" + ", ".join(binance_val_list)
 		file.write(line)
 
 def start_process():
@@ -52,6 +59,7 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=token)
 dp = Dispatcher(bot)
 
+prices = client.get_all_tickers()
 
 async def send():
 	global settings_info_line, settings_info
@@ -68,6 +76,18 @@ async def send():
 			settings_info_line = update_info_line()
 			write_into_file()
 			await bot.send_message(my_id, "Flags updated")
+
+	async def check_binance():
+		global client, settings_info, binance_val_list
+		prices = client.get_all_tickers()
+
+		line = ""
+		for inf in prices:
+			if inf['symbol'] in binance_val_list:
+				line += inf['symbol'] + " last cost is " + str(inf['price']) + "\n"
+
+		if line:
+			await bot.send_message(my_id, line)
 
 	async def check_sending():
 		global settings_info_line, settings_info
@@ -104,7 +124,11 @@ async def send():
 	schedule.every().thursday.at(cur_time).do(check_sending)
 	schedule.every().friday.at(cur_time).do(check_sending) 
 
-	schedule.every().day.at("00:01").do(update_flags) 
+	schedule.every().day.at("9:00").do(check_binance)
+	schedule.every().day.at("16:00").do(check_binance)
+	schedule.every().day.at("23:00").do(check_binance)
+
+	schedule.every().day.at("23:00").do(update_flags)
 	while True:
 		await schedule.run_pending()
 		await asyncio.sleep(60)
@@ -123,15 +147,23 @@ changing_settings = "None"
 calculate_readline = False
 
 another_set = False
+hists =  {'cities': False, 'binance': False}
 with codecs.open("send_data.txt", encoding = 'utf-8', mode = 'r') as file:
 	line = file.readline()
 	while line:
 		if clear_line(line) != "":
-			if clear_line(line) == "-" or another_set:
-				if clear_line(line) == "-":
+			if clear_line(line)[0] == "-" or another_set:
+				if clear_line(line) == "-1":
 					another_set = True
+					hists['cities'] = True
+				elif clear_line(line) == "-2":
+					hists['cities'] = False
+					hists['binance'] = True
 				else:
-					cities_history = clear_line(line).split(",")
+					if hists['cities']:
+						cities_history = clear_line(line).split(",")
+					elif hists['binance']:
+						binance_val_list = clear_line(line).split(",")
 
 			else:
 				sup_arr = line.split(": ", 1)
@@ -145,10 +177,11 @@ settings_info_line = update_info_line()
 keyboard_main = types.ReplyKeyboardMarkup(resize_keyboard=True)
 keyboard_main.add(types.KeyboardButton(text="Настройки"), types.KeyboardButton(text="Отправить"))
 keyboard_main.add(types.KeyboardButton(text="Кулькулятор"), types.KeyboardButton(text="Погода"))
-keyboard_main.add(types.KeyboardButton(text="Living time"))
+keyboard_main.add(types.KeyboardButton(text="Living time"), types.KeyboardButton(text="Binance Info"))
 
 keyboard_bin = types.ReplyKeyboardMarkup(resize_keyboard=True)
 keyboard_bin.add(types.KeyboardButton(text="1"), types.KeyboardButton(text="0"))
+keyboard_bin.add(types.KeyboardButton(text="Main"))
 
 keyboard_cities = types.ReplyKeyboardMarkup(resize_keyboard=True)
 def create_keyboard_cities():
@@ -157,7 +190,21 @@ def create_keyboard_cities():
 	for k in range(len(cities_history)//2):
 		keyboard_cities.add(types.KeyboardButton(text=cities_history[k*2]), types.KeyboardButton(text=cities_history[k*2 + 1]))
 	if len(cities_history) % 2 != 0: keyboard_cities.add(types.KeyboardButton(text=cities_history[-1]))
+
+	keyboard_cities.add(types.KeyboardButton(text="Main"))
+
+keyboard_binance = types.ReplyKeyboardMarkup(resize_keyboard=True)
+def create_keyboard_binance():
+	global keyboard_binance
+	keyboard_binance = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	for k in range(len(binance_val_list)//2):
+		keyboard_binance.add(types.KeyboardButton(text=binance_val_list[k*2]), types.KeyboardButton(text=binance_val_list[k*2 + 1]))
+	if len(binance_val_list) % 2 != 0: keyboard_binance.add(types.KeyboardButton(text=binance_val_list[-1]))
+
+	keyboard_binance.add(types.KeyboardButton(text="Main"))
+
 create_keyboard_cities()
+create_keyboard_binance()
 
 keyboard_settings = types.ReplyKeyboardMarkup(resize_keyboard=True)
 set_keys = [*settings_info.keys()]
@@ -181,21 +228,33 @@ async def cmd_start(message: types.Message):
 async def echo(message: types.Message):
 	global change_settings, settings_info_line, settings_info
 	global changing_settings, calculate_readline, cities_history
+	global binance_val_list, clinet
 
 	canonical_command = message.text
 	command = canonical_command.lower()
 	
 	if change_settings:
 		change_settings = False
-		settings_info[changing_settings] = command
+		if command != "main":
+			settings_info[changing_settings] = command
 
 		if (changing_settings == "City"):
 			if command.title() not in cities_history:
-				cities_history = [command.title(), *cities_history[:-1]]
+				if command.title() != "Main":
+					cities_history = [command.title(), *cities_history[:-1]]
 			else:
 				cities_history.remove(command.title())
 				cities_history = [command.title(), *cities_history]
 			create_keyboard_cities()
+		elif (changing_settings == "Binance Currency"):
+			settings_info[changing_settings] = command.upper()
+			if command.upper() not in binance_val_list:
+				if command.title() != "Main":
+					binance_val_list = [command.upper(), *binance_val_list[:-1]]
+			else:
+				binance_val_list.remove(command.upper())
+				binance_val_list = [command.upper(), *binance_val_list]
+			create_keyboard_binance()
 
 		settings_info_line = update_info_line()
 		await message.answer(settings_info_line, reply_markup=keyboard_main)
@@ -253,6 +312,15 @@ async def echo(message: types.Message):
 			res -= (res // k) * k
 
 		await message.answer(res_str)
+	elif command == "binance info":
+		prices = client.get_all_tickers()
+
+		line = "No any matches"
+		for inf in prices:
+			if inf['symbol'] == settings_info["Binance Currency"]:
+				line = settings_info["Binance Currency"] + " last cost is " + str(inf['price'])
+
+		await message.answer(line)
 	elif command == "main":
 		await message.answer("Openning main keyboard", reply_markup=keyboard_main)
 	elif message.text in settings_info.keys():
@@ -260,9 +328,12 @@ async def echo(message: types.Message):
 		changing_settings = canonical_command 
 		if changing_settings == "City":
 			await message.answer("Changing <<" + changing_settings + ">> setting", reply_markup=keyboard_cities)
+		if changing_settings == "Binance Currency":
+			await message.answer("Changing <<" + changing_settings + ">> setting", reply_markup=keyboard_binance)
 		elif changing_settings in ["File Send", "Sending Activate"]:
 			buttons = types.InlineKeyboardMarkup()
 			buttons.add(types.InlineKeyboardButton('1', callback_data = '1'), types.InlineKeyboardButton('0', callback_data = '0'))
+			buttons.add(types.InlineKeyboardButton('Main', callback_data = 'Main'))
 			await message.answer("Changing <<" + changing_settings + ">> setting", reply_markup=buttons)
 		else:
 			await message.answer("Changing <<" + changing_settings + ">> setting")
@@ -272,16 +343,22 @@ async def process_callback_button1(callback_query: types.CallbackQuery):
 	global change_settings, settings_info, settings_info_line
 
 	data = callback_query.data
-
-	change_settings = False
-	settings_info[changing_settings] = data
-
-	settings_info_line = update_info_line()
-
-	await bot.answer_callback_query(callback_query.id)
-	await bot.send_message(callback_query.from_user.id, settings_info_line, reply_markup=keyboard_main)
 	
-	write_into_file()
+	change_settings = False
+
+	if data != 'Main':
+		
+		settings_info[changing_settings] = data
+
+		settings_info_line = update_info_line()
+
+		await bot.answer_callback_query(callback_query.id)
+		await bot.send_message(callback_query.from_user.id, settings_info_line, reply_markup=keyboard_main)
+
+		write_into_file()
+
+	else:
+		await bot.send_message(callback_query.from_user.id, "Back to main", reply_markup=keyboard_main)
 
 
 start_time = time.time()
